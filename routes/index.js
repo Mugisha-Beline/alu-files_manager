f339a818305213943f30db8404ea2744e51fbb08
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-13 17:21:08 +0200

    ƒix eslint errors

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
+/* eslint-disable jest/require-hook */
 import express from 'express';
 import AppController from '../controllers/AppController';
 
 const router = express.Router();
-
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
