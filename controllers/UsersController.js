009717657f53b16923400fc3cab8927088fea849
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-13 17:36:30 +0200

    resolve eslint errors

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,9 @@
 export default class UsersController {
-
+  static postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    return 4;
+  }
 }
