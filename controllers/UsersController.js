cb0ef368578c802f39585a4f4b0ae4efa8982576
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-13 17:44:46 +0200

    add 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,26 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
 export default class UsersController {
-  static postNew(req, res) {
+  static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email || !password) {
+    if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
-    return 4;
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+    const user = await dbClient.usersCollection.findOne({ email });
+    console.log(user);
+    if (user) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+    const hashedPassword = sha1(password);
+    const result = await dbClient.usersCollection.insertOne({ email, password: hashedPassword });
+    const createdUser = {
+      id: result.insertedId,
+      email,
+    };
+    return res.status(201).json(createdUser);
   }
 }
