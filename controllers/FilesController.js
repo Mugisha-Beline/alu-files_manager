ed0834b0225baa064d871695ad3084983009f2ab
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-17 10:11:46 +0200

    modify

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -78,6 +78,7 @@ export default class FilesController {
       name: req.body.name,
       type: req.body.type,
       parentId,
+      isPublic: req.body.isPublic || false,
     };
     return res.status(201).json(returnItem);
   }
