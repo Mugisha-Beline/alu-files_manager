4b38dd8b841d589a163aebaf006c990a7f7393b1
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-21 13:39:54 +0200

    modify 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -147,9 +147,9 @@ export default class FilesController {
     console.log('skip', skip);
 
     const query = { };
-    if (parentId !== 0 && parentId !== '0') {
-      query.parentId = ObjectID(parentId);
-    }
+    // if (parentId !== 0 && parentId !== '0') {
+    query.parentId = ObjectID(parentId);
+    // }
 
     const files = await dbClient.filesCollection
       .find(query)
