cf209df934c3cffc580188908083873f2f88c512
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-21 15:16:40 +0200

    add unpublish

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -178,4 +178,34 @@ export default class FilesController {
       parentId: file.parentId,
     });
   }
+
+  static async putUnpublish(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const fileId = req.params.id;
+    const file = await dbClient.filesCollection.findOne({
+      _id: ObjectID(fileId),
+      userId: ObjectID(userId),
+    });
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+    file.isPublic = false;
+    await dbClient.filesCollection.updateOne({ _id: ObjectID(fileId) },
+      { $set: { isPublic: false } });
+    return res.status(200).json({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    });
+  }
 }
